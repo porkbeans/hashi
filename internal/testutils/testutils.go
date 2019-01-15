a3b4dd9a528fc7ae7d118e355b1569899eab5b51
porkbeans <mizuo.taka@gmail.com>
2019-01-15 21:22:16 +0900

    Fix for lint

--- internal/testutils/testutils.go
+++ internal/testutils/testutils.go
@@ -8,41 +8,46 @@ import (
 	"net/url"
 )
 
-func GenerateRandomBytes() string {
+func generateRandomBytes() string {
 	buf := [32]byte{}
 	rand.Read(buf[:])
 	return string(buf[:])
 }
 
+// GenerateInvalidURL provides random invalid URL
 func GenerateInvalidURL() string {
 	var rawURL string
 	var err error
 
 	for err == nil {
-		rawURL = GenerateRandomBytes()
+		rawURL = generateRandomBytes()
 		_, err = url.Parse(rawURL)
 	}
 
 	return rawURL
 }
 
+// TestServerHandler is a handler that returns specified contents for tests
 type TestServerHandler struct {
 	StatusCode int
 	Content    string
 }
 
+// ServeHTTP returns specified contents in TestServerHandler
 func (h TestServerHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(h.StatusCode)
 	writer.Header().Add("Content-Type", "text/plain")
 	writer.Write([]byte(h.Content))
 }
 
+// TouchTempFile creates a temporary file
 func TouchTempFile() (string, error) {
 	file, err := ioutil.TempFile("", "hashi-test-")
 	defer file.Close()
 	return file.Name(), err
 }
 
+// CreateTempZip creates a zip file that contains a file
 func CreateTempZip(filenameInZip, content string) (string, error) {
 	zipFile, err := ioutil.TempFile("", "hashi-test-")
 	if err != nil {
